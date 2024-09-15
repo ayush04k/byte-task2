c2f1597314891aeddb7e6a2c1e308b23a0e3cf00
Ayush Kumar <139777742+ayush04k@users.noreply.github.com>
2024-09-15 19:43:42 +0530

    Update auth.js

--- server/auth.js
+++ server/auth.js
@@ -9,7 +9,7 @@ const router = express.Router();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: process.env.NODE_ENV === 'production' ? `https://byte-task2.vercel.app/auth/google/callback` : `http://localhost:3000/auth/google/callback`
 }, (accessToken, refreshToken, profile, done) => {
   // Store the access token for later use
   console.log('Google Strategy Callback');
